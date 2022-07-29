34f02e48e983f8be21df3419230e5bb5496d5ec1
Sean <seanmcardle14@gmail.com>
2022-07-28 22:47:00 -0500

    added simple CRTBP EOM

--- src/AstroEOM.jl
+++ src/AstroEOM.jl
@@ -1,5 +1,27 @@
 module AstroEOM
 
-# Write your package code here.
+function eom_crtbp(du, u, p, t)
+
+    x, y, z, vx, vy, vz = u
+    μ = p[1]
+
+    # Distance from primary
+    x1 = x+μ
+    r1 = sqrt(x1*x1+y*y+z*z)
+
+    # Distance from secondary
+    x2 = x+μ-1.0
+    r2 = sqrt(x2*x2+y*y+z*z)
+
+    du[1] = vx
+    du[2] = vy
+    du[3] = vz
+    du[4] =  2.0*vy + x - (1.0-μ)*x1/r1^3 - μ*x2/r2^3
+    du[5] = -2.0*vx + y - (1.0-μ)*y/r1^3  - μ*y/r2^3
+    du[6] =              -(1.0-μ)*z/r1^3  - μ*z/r2^3
+
+end
+
+export eom_crtbp
 
 end
--- test/runtests.jl
+++ test/runtests.jl
@@ -2,5 +2,13 @@ using AstroEOM
 using Test
 
 @testset "AstroEOM.jl" begin
-    # Write your tests here.
+
+    u = [1.1, 0.0, 0.0, 0.0, 0.1, 0.0]
+    p = [0.01214]
+    t = 0.0
+    du = zeros(6)
+    eom_crtbp(du, u, p, t)
+
+    @test isapprox(du, [0.0, 0.1, 0.0, -0.46406516020019184, 0.0, -0.0], rtol=1e-15)
+
 end
